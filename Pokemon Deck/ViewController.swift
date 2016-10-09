e2449b8630671e6bb62910c0d69d9f929dd41689
Louis <Louis@Louiss-MacBook-Air.local>
2016-10-09 13:00:52 +0800

    Collection view delegate, data source and flow layout

--- Pokemon Deck/ViewController.swift
+++ Pokemon Deck/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collection: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collection.dataSource = self
+        collection.delegate = self
         
+    }
+    
+    // deque cell and set it up
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            
+            let pokemon = Pokemon(name: "Pokemon", deckID: indexPath.row)
+            cell.configureCell(pokemon: pokemon)
+            
+            return cell
+            
+        } else {
+            
+            return UICollectionViewCell()
+        }
+    }
+    
+    // execute when tapped into the cell
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    // number of items in the section
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    // number of sections in the collection view
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    // define the size of the cells
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 105, height: 105)
     }
 
 }
