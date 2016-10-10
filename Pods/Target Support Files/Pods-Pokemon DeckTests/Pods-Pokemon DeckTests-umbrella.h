d3fc7346d05d674dbb01bd82b240ca33e9c3096f
Louis <Louis@Louiss-MacBook-Air.local>
2016-10-10 10:38:39 +0800

    Install Alamofire

---
+++ Pods/Target Support Files/Pods-Pokemon DeckTests/Pods-Pokemon DeckTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Pokemon_DeckTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Pokemon_DeckTestsVersionString[];
+
---
+++ Pods/Target Support Files/Pods-Pokemon DeckUITests/Pods-Pokemon DeckUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pokemon_DeckUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Pokemon_DeckUITests
+@end
