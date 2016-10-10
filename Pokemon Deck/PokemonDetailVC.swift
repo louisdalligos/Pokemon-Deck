392e80fe4c7be868302615363657e163e1c0532e
Louis <Louis@Louiss-MacBook-Air.local>
2016-10-10 10:01:28 +0800

    IBOutlet and model changes

--- Pokemon Deck/PokemonDetailVC.swift
+++ Pokemon Deck/PokemonDetailVC.swift
@@ -14,11 +14,31 @@ class PokemonDetailVC: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel!
     
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedeckLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         nameLabel.text = pokemon.name
 
+    }
+    
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        
     }
 
 
